32ee0f5172d8d715af28f334f7bb5072bf7fb9ed
hshi44 <48733002+hshi44@users.noreply.github.com>
2021-04-26 17:02:34 -0500

    Update HighMOIFixedAsianCommets.R
    Comment spelling check and lines for plotting

--- HighMOIFixedAsianCommets.R
+++ HighMOIFixedAsianCommets.R
@@ -24,7 +24,7 @@ fixed_delay_model <- function(time, y, parms){
   })
 }
 
-##Defineing the function ssq to integrate and evaluate the prediction error of parameter set p##
+##Defining the function ssq to integrate and evaluate the prediction error of parameter set p##
 ssq <- function(p){
   out <- dede(y0,t,fixed_delay_model,p)
   prd <- data.frame(out)
@@ -33,11 +33,11 @@ ssq <- function(p){
   return(res)
 }
 
-##Reading experimetnal data from file(s)##
+##Reading experimental data from file(s)##
 counts <- read.csv(paste0(DVdir,'CountsforHighMOIModel.csv'))
 virus <- data.frame(counts[,c(1,3)])
 colnames(virus) <- c("time","V")
-##Time rage of the intergration##
+##Time rage of the integration##
 t <- c(seq(0,64,length=200),virus$time)
 t <- sort(unique(t))
 ##Defining initial conditions##
@@ -50,7 +50,7 @@ put <- NULL
 
 k6=0.06470
 
-##Parameter fitting. Different strating values can be set by external input or for loops##
+##Parameter fitting. Different starting values can be set by external input or for loops##
 #for(j in 1:10){
 #  for(l in 1:10){
     parms<-c(k4=5.4e-2,k5=4.2,tau=11.3)
@@ -64,5 +64,11 @@ k6=0.06470
 #  }
 #}
 
+##Plotting the predicted virus growth curve with optimized parameters. The dots represent experimental data. The y-axis is on the log scale. Write the output virus and cell kinetics as a file. Comment these lines out if the script is used for parameter optimization only.##
+prd <- data.frame(out)
+plot(prd$time,prd$V,type = 'l',log="y",xlab = 'Time (h.p.i.)',ylab = 'Virus Titer (PFU/mL)',ylim = c(1e1,1e8))
+points(virus)
+write.csv(put,file = 'OSFD_Asian_modeloutput.csv')
+
 ##The parameters and the evaluation as the output##
-write.csv(put,file = paste0(DVdir,'OSFD_Asian.csv'))
+write.csv(put,file = paste0(DVdir,'OSFD_Asian_parameters.csv'))
